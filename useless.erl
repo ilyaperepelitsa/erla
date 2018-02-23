3282e2f9c014a843ccbd1c6c4b0211cd2881a8ec
Ilya Perepelitsa <ilya@dataslap.com>
2018-02-23 01:47:25 -0500

    Updated useless.erl.

--- useless.erl
+++ useless.erl
@@ -7,7 +7,7 @@ add(A,B) ->
   A + B.
 
 hello() ->
-  io:format("Hello, world!~n")
+  io:format("Hello, world!~n").
 
 %% Shows greetings.
 %% io:format/1 is the standard function used to output text.
