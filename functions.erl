0651e727ce8433bb7eed6eafffc4a420273901e2
Ilya Perepelitsa <ilya@dataslap.com>
2018-02-25 02:04:33 -0500

    Updated functions.erl.

--- functions.erl
+++ functions.erl
@@ -18,5 +18,6 @@ valid_time(_) ->
   io:format("Stop feeding me wrong data!~n").
 
 
-old_enough(X) when X >= 16, X=< 104 -> true;
+old_enough(X) when X >= 16, X=< 104 ->
+  true;
 old_enough(_) -> false.
