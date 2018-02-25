a6aad35e3beb529282c500e100b49c2ba94fb4c3
Ilya Perepelitsa <ilya@dataslap.com>
2018-02-25 01:59:35 -0500

    Updated functions.beamfunctions.erl .

--- functions.erl
+++ functions.erl
@@ -16,3 +16,6 @@ valid_time({Date = {Y, M, D}, Time = {H, Min, S}}) ->
   io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time, H, Min, S]);
 valid_time(_) ->
   io:format("Stop feeding me wrong data!~n").
+
+
+old
