9df81b0c89dc479d4408d5b0e6f0d375e655c156
Ilya Perepelitsa <ilya@dataslap.com>
2018-02-25 01:22:16 -0500

    Updated functions.erl.

--- functions.erl
+++ functions.erl
@@ -14,4 +14,4 @@ same(_, _) ->
 valid_time({Date = {Y, M, D}, Time = {H, Min, S}}) ->
   io:format("The Date tuple (~p) says today is ~p/~p/~p,~n", [Date, Y, M, D]),
   io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time, H, Min, S]);
-valid_time()
+valid_time(_)
